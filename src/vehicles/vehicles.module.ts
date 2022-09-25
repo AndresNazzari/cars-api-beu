66de93690c3352dbc383c42ba31aeec4a1c81ee5
Andres Nazzari <lordcliff@gmail.com>
2022-09-25 08:21:03 -0300

    ✨ NEW FEATURE: Update vehicle module

--- src/vehicles/vehicles.module.ts
+++ src/vehicles/vehicles.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { VehiclesController } from './controllers/vehicles.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Brand } from 'src/entities/Brand.entity';
+import { Color } from 'src/entities/Color.entity';
+import { Vehicle } from 'src/entities/Vehicle.entity';
 import { VehiclesService } from './services/vehicles.service';
+import { ColorsService } from 'src/colors/services/colors.service';
+import { BrandsService } from 'src/brands/services/brands.service';
+import { VehiclesController } from './controllers/vehicles.controller';
+import { ColorsController } from 'src/colors/controllers/colors.controller';
+import { BrandsController } from 'src/brands/controllers/brands.controller';
 
 @Module({
-  controllers: [VehiclesController],
-  providers: [VehiclesService]
+  imports: [TypeOrmModule.forFeature([Vehicle, Brand, Color])],
+  providers: [VehiclesService, ColorsService, BrandsService],
+  controllers: [VehiclesController, ColorsController, BrandsController],
 })
 export class VehiclesModule {}
