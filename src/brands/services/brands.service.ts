736b17e3ebbbdee1e40bbe1f25877c000741d3d4
Andres Nazzari <lordcliff@gmail.com>
2022-09-25 08:12:21 -0300

    ✨ NEW FEATURE: Add services to brands CRUD

--- src/brands/services/brands.service.ts
+++ src/brands/services/brands.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Brand } from '../../entities/Brand.entity';
 
 @Injectable()
-export class BrandsService {}
+export class BrandsService {
+  constructor(
+    @InjectRepository(Brand)
+    private brandRepository: Repository<Brand>,
+  ) {}
+
+  async findAll() {
+    return await this.brandRepository.find();
+  }
+
+  async findOne(id: number) {
+    return await this.brandRepository.findOne({ where: { id } });
+  }
+
+  async findOneByName(name: string) {
+    return await this.brandRepository.findOne({ where: { name } });
+  }
+
+  async createNewBrand(body: any) {
+    const newBrand = this.brandRepository.create(body);
+    return await this.brandRepository.save(newBrand);
+  }
+
+  async update(brand: Brand, body: any) {
+    this.brandRepository.merge(brand, body);
+    return await this.brandRepository.save(brand);
+  }
+
+  async delete(id: number) {
+    return await this.brandRepository.softDelete({ id });
+  }
+}
