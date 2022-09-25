a7f9b24e0667b927f4cee7389ba9441a1b38ef0d
Andres Nazzari <lordcliff@gmail.com>
2022-09-25 08:22:34 -0300

    🗄️ DB: update with typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,35 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ColorsModule } from './colors/colors.module';
 import { BrandsModule } from './brands/brands.module';
 import { VehiclesModule } from './vehicles/vehicles.module';
+import { Color } from './entities/Color.entity';
+import { Brand } from './entities/Brand.entity';
+import { Vehicle } from './entities/Vehicle.entity';
+import { SeedsModule } from './seeds/seeds.module';
 
 @Module({
-  imports: [ColorsModule, BrandsModule, VehiclesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forFeature([Color, Brand, Vehicle]),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: process.env.POSTGRES_USER || 'root',
+      password: process.env.POSTGRES_PASSWORD || 'password',
+      database: process.env.POSTGRES_DB || 'cars-api',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+      retryDelay: 3000,
+      retryAttempts: 10,
+    }),
+    ColorsModule,
+    BrandsModule,
+    VehiclesModule,
+    SeedsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
