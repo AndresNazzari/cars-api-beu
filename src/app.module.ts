2b7d07c5e9fd624b3e0f9cc79e2c330ebe1eadbf
Andres Nazzari <lordcliff@gmail.com>
2022-09-25 08:07:53 -0300

    👌 UPDATE: update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ColorsModule } from './colors/colors.module';
+import { BrandsModule } from './brands/brands.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
-  imports: [ColorsModule],
+  imports: [ColorsModule, BrandsModule, VehiclesModule],
   controllers: [AppController],
   providers: [AppService],
 })
