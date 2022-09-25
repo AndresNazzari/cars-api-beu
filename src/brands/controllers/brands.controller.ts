e663d400c95fe0ae68a98e73afb791f85886b39a
Andres Nazzari <lordcliff@gmail.com>
2022-09-25 08:14:13 -0300

    👍 DONE: Add post put delete to brand controller

--- src/brands/controllers/brands.controller.ts
+++ src/brands/controllers/brands.controller.ts
@@ -48,4 +48,81 @@ export class BrandsController {
         .json({ msg: 'An error has ocurred', error: error.message });
     }
   }
+
+  @Post('')
+  async addNewBrand(@Body() body: BrandDto, @Res() res: Response) {
+    if (!body.name) {
+      return res.status(400).json({ msg: 'Name is required', data: {} });
+    }
+
+    try {
+      const brandExists = await this.brandService.findOneByName(body.name);
+      if (brandExists) {
+        return res.status(409).json({
+          msg: `A brand found with name ${body.name}. Brand name must be unique.`,
+          data: brandExists,
+        });
+      }
+      const brandSaved = await this.brandService.createNewBrand(body);
+      return res.status(201).json({ msg: 'Success', data: brandSaved });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ msg: 'An error has ocurred', error: error.message });
+    }
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() body: BrandDto,
+    @Res() res: Response,
+  ) {
+    if (!body.name) {
+      return res.status(400).json({ msg: 'Name is required', data: {} });
+    }
+    try {
+      const brandById = await this.brandService.findOne(id);
+      const brandByName = await this.brandService.findOneByName(body.name);
+
+      if (!brandById) {
+        return res
+          .status(404)
+          .json({ msg: `No brand found with id ${id}`, data: {} });
+      }
+
+      if (brandByName) {
+        return res.status(409).json({
+          msg: `A brand found with name ${body.name}. Brand name must be unique.`,
+          data: brandById,
+        });
+      }
+
+      const brandUpdated = await this.brandService.update(brandById, body);
+      return res.status(200).json({ msg: 'Success', data: brandUpdated });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ msg: 'An error has ocurred', error: error.message });
+    }
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number, @Res() res: Response) {
+    try {
+      const brandById = await this.brandService.findOne(id);
+      if (!brandById) {
+        return res
+          .status(404)
+          .json({ msg: `No brand found with id ${id}`, data: {} });
+      }
+      await this.brandService.delete(id);
+
+      return res.status(200).json({ msg: 'Success', data: brandById });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ msg: 'An error has ocurred', error: error.message });
+    }
+  }
 }
