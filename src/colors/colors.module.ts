301bf49050e3810f142122ff7f45978968b9ceb5
Andres Nazzari <lordcliff@gmail.com>
2022-09-25 08:19:41 -0300

    ✨ NEW FEATURE: update colors module

--- src/colors/colors.module.ts
+++ src/colors/colors.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ColorsController } from './controllers/colors.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Color } from '../entities/Color.entity';
 import { ColorsService } from './services/colors.service';
-
+import { ColorsController } from './controllers/colors.controller';
 @Module({
+  imports: [TypeOrmModule.forFeature([Color])],
   controllers: [ColorsController],
-  providers: [ColorsService]
+  providers: [ColorsService],
 })
 export class ColorsModule {}
