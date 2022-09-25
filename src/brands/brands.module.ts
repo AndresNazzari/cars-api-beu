4d37b476c0795fac1109dbfa6c4126ccb19a045d
Andres Nazzari <lordcliff@gmail.com>
2022-09-25 08:18:24 -0300

    👍 DONE: Add

--- src/brands/brands.module.ts
+++ src/brands/brands.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Brand } from '../entities/Brand.entity';
 import { BrandsService } from './services/brands.service';
 import { BrandsController } from './controllers/brands.controller';
-
 @Module({
+  imports: [TypeOrmModule.forFeature([Brand])],
   providers: [BrandsService],
-  controllers: [BrandsController]
+  controllers: [BrandsController],
 })
 export class BrandsModule {}
