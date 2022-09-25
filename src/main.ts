a4e99abf6559dc3399a7467a5b0e557b98455fea
Andres Nazzari <lordcliff@gmail.com>
2022-09-25 08:24:10 -0300

    📝 DOC: Update for swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Cars API for BEU.app')
+    .setDescription('API for BEU.app')
+    .setVersion('1.0')
+    .addTag('Cars Api')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api/docs', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
