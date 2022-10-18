027dd2d7fedc28460e26a512d16d27af9a168ff8
Andres Nazzari <lordcliff@gmail.com>
2022-10-18 18:49:31 +0900

    ➕ ADD DEP: env variables config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ColorsModule } from './colors/colors.module';
 import { BrandsModule } from './brands/brands.module';
@@ -10,11 +11,12 @@ import { SeedsModule } from './seeds/seeds.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forFeature([Color, Brand, Vehicle]),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'postgres',
-      port: 5432,
+      host: process.env.POSTGRES_HOST || 'postgres',
+      port: parseInt(process.env.POSTGRES_PORT) || 5432,
       username: process.env.POSTGRES_USER || 'root',
       password: process.env.POSTGRES_PASSWORD || 'password',
       database: process.env.POSTGRES_DB || 'cars-api',
